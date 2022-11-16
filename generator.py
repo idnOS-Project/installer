18adeece2db849774e5ffb032a1cc9db89b0a154
Iyxan23 <nurihsanalghifari@gmail.com>
2022-11-16 21:02:10 +0700

    wip: pipe the sources list directly

--- generator.py
+++ generator.py
@@ -1,6 +1,14 @@
 # The rootfs generator, this script can only be run on a debian environment
 # as it uses `mmdebstrap`, which is only available on debian distros
 
+import shutil
+import sys
+
+# check if mmdebstrap exists
+if shutil.which("mmdebstrap") is None:
+    print("mmdebstrap does not exists", file=sys.stderr)
+    exit(1)
+
 import subprocess
 import tomllib
 
@@ -9,15 +17,14 @@ config = None
 with open("config.toml", "rb") as f:
     config = tomllib.load(f)
 
-with open("sources_list.txt", "rw") as f:
-    f.write("\n".join(config["mmdebstrap"]["sources-list"]))
-
-subprocess.check_call(
-    ["mmdebstrap",
+output = subprocess.run(
+    [
+     "mmdebstrap",
      "--architectures", config["mmdebstrap"]["arch"],
      "--verbose",
      "--include=", "m".join(config["mmdebstrap"]["packages"]),
      config["mmdebstrap"]["outdir"],
-     "sources_list.txt"
-     ]
+    ],
+    input="\n".join(config["mmdebstrap"]["sources-list"]),
+    text=True
 )
